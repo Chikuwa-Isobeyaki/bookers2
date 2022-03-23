be681f38529d3aa170a539c91f73a3595f6f11e4
EC2 Default User <ec2-user@ip-172-31-43-167.ec2.internal>
2022-03-23 07:23:37 +0000

    bookの,index,showページを作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,33 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
+    
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
+    @book_of = Book.find(params[:id])
+    @book = Book.new
+  end
+  
+   private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
+
+
+
+
