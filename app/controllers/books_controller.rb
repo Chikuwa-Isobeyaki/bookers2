e6ec4ad4fe2ea6caf1cfd838d3e92c7121f7551c
EC2 Default User <ec2-user@ip-172-31-43-167.ec2.internal>
2022-03-29 09:10:51 +0000

    [Finish]テスト完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  
   def index
     @book = Book.new
     @books = Book.all
@@ -24,6 +25,11 @@ class BooksController < ApplicationController
   
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
   end
   
   
@@ -46,6 +52,8 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
+  
 end
 
 
